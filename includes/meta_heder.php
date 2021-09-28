a64cba6fcd494f77d53470fc9deb125c3717dead
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-28 22:08:29 +0200

    img

--- includes/meta_heder.php
+++ includes/meta_heder.php
@@ -1,4 +1,5 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css">
+<link href="../includes/img.js" rel="stylesheet" type="js">
 <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
 <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css'>
 <link rel="icon" type="image/png" href="/sistemes_dam1/images/logo.png" />
