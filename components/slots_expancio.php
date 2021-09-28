6d2872343d63b5d4551d659496beaaf86ff2af95
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-28 17:18:57 +0200

    g

--- components/slots_expancio.php
+++ components/slots_expancio.php
@@ -9,6 +9,12 @@
         <?php include ('../includes/menu.php') ?>
         <h1 class="margin_top"></h1>
         <!--index-->
+        <div class="center">
+                <div class="pagination">
+                    <a href="#" class="fletxas"><!--&laquo;--></a>
+                    <a href="jornada2.php" class="fletxas">Jornada 2 &raquo; </a>
+                </div>
+        </div>
         <p class="index"><a href="components/slots_expancio.php">Slots d’Expansió a la PB + Placa</a></p>
         <p class="index"><a href="">Disc + Connexions Integrades Internes de Disc Dur + Placa</a></p>
     </body>
