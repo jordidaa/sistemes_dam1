046e093eabfe3511da51440a20a9b91f97608ef8
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-28 22:05:13 +0200

    Update slots_expancio.php

--- components/slots_expancio.php
+++ components/slots_expancio.php
@@ -15,8 +15,21 @@
                     <a href="jornada2.php" class="fletxas">Jornada 2 &raquo; </a>
                 </div>
         </div>
-        <p class="index"><a href="components/slots_expancio.php">Slots d’Expansió a la PB + Placa</a></p>
-        <p class="index"><a href="">Disc + Connexions Integrades Internes de Disc Dur + Placa</a></p>
+        <!-- Trigger the Modal -->
+        <img id="myImg" src="../images/pb_sf.jpg" alt="Snow" style="width:100%;max-width:300px">
+
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img class="modal-content" id="img01">
+
+          <!-- Modal Caption (Image Text) -->
+          <div id="caption"></div>
+        </div>
     </body>
     <?php include ('../includes/meta_footer.php') ?>
 </html>
