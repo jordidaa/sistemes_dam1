464e5e4fa805d114ee6f57187058ca409306c87f
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-28 22:58:51 +0200

    Update slots_expancio.php

--- components/slots_expancio.php
+++ components/slots_expancio.php
@@ -27,13 +27,13 @@
         </div>
         <h1 class="titolcp">PCIe x16</h1>
         <div class="col-sm-5 col-md-6"><div class="imgcent">
-            <img id="myImg2" class="imgcent1" src="../images/pb_sf.jpg" alt="Placa base" style="width:100%;max-width:500px">
+            <img id="myImg" class="imgcent1" src="../images/pb_sf.jpg" alt="Placa base" style="width:100%;max-width:500px">
 
             <!-- The Modal -->
-            <div id="myModal2" class="modal">
+            <div id="myModal" class="modal">
               <span class="close">&times;</span>
-              <img class="modal-content" id="img02">
-              <div id="caption2"></div>
+              <img class="modal-content" id="img01">
+              <div id="caption"></div>
             </div>
         </div></div>
         <div class="col-sm-5 col-md-6"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor orci id lorem rhoncus, vitae euismod ex vestibulum. Integer scelerisque, lacus vel finibus feugiat, nisl nulla pulvinar odio, aliquam dignissim augue turpis hendrerit lorem. Aenean sodales ante eu vulputate sodales. Sed suscipit odio a ullamcorper congue. Maecenas sit amet justo elit. Praesent feugiat massa et mi placerat consequat vitae eget velit. Curabitur volutpat semper erat suscipit vulputate. Quisque finibus tempus commodo. Ut neque metus, eleifend in ultrices convallis, porttitor at eros. In semper ligula sem, quis sagittis ipsum tincidunt vitae. Nullam consequat sapien et ipsum tincidunt, a aliquam sapien tristique. Phasellus non erat a arcu aliquam consectetur. In hac habitasse platea dictumst. Donec vulputate lectus at tortor ullamcorper malesuada.
@@ -65,28 +65,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 </script>
-<script>
-// Get the modal
-var modal = document.getElementById("myModal2");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg2");
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption2");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-</script>
     </body>
     <?php include ('../includes/meta_footer.php') ?>
 </html>
