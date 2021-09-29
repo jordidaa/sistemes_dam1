026b212793f714cd0851216093951e470a776b89
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-29 18:38:25 +0200

    Update disc_intern.php

--- components/disc_intern.php
+++ components/disc_intern.php
@@ -109,6 +109,72 @@
         <div class="col-sm-5 col-md-6 cent md">
           <img id="myImg" class="imgcent2" src="../images/sata_3.png" alt="Placa base" style="width:100%;max-width:250px">
         </div>
+        <h1 class="titolcp">Disc dur M.2</h1>
+        <div class="col-sm-5 col-md-6 cent md">
+          <img id="myImg" class="imgcent2" src="../images/m_2_dd.png" alt="Placa base" style="width:100%;max-width:250px">
+        </div>
+        <div class="col-sm-5 col-md-6 cent md">
+          <p> 
+          El M.2 és la connexió de Disc dur amb placa base més ràpida que hi ha en el mercat, va a una velocitat de 6 Gbit/s.
+          </p>
+
+        <!-- Trigger the modal with a button -->
+          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal3">Veure imatge gran</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal3" role="dialog">
+          <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Disc dur M.2</h4>
+              </div>
+              <div class="modal-body">
+              <img id="myImg" class="imgcentbtn" src="../images/m_2_dd.png" alt="Placa base" style="width:100%;max-width:600px"> 
+              </div>
+              <!--<div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>-->
+            </div>
+
+          </div>
+        </div>
+        </div>
+        <h1 class="titolcp">SATA 3</h1>
+        <div class="col-sm-5 col-md-6 cent md">
+        <p>
+        El SATA 3 va a la mateixa velocitat que M.2 pero perd velocitat perque sha de conectar a partir de un cable.<br>
+        Va a una velocitat de 6 Gbit/s.
+        </p>
+        <!-- Trigger the modal with a button -->
+          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Veure imatge gran</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal2" role="dialog">
+          <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">SATA 3</h4>
+              </div>
+              <div class="modal-body">
+              <img id="myImg" class="imgcentbtn" src="../images/sata_3.png" alt="Placa base" style="width:100%;max-width:600px"> 
+              </div>
+              <!--<div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>-->
+            </div>
+
+          </div>
+        </div>
+        </div>
+        <div class="col-sm-5 col-md-6 cent md">
+          <img id="myImg" class="imgcent2" src="../images/sata_3.png" alt="Placa base" style="width:100%;max-width:250px">
+        </div>
     </body>
     <?php include ('../includes/meta_footer.php') ?>
 </html>
