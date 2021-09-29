f05375528fccabe730e4677e8649cb3ae7fa4b8b
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-09-29 07:33:39 +0200

    Update slots_expancio.php

--- components/slots_expancio.php
+++ components/slots_expancio.php
@@ -29,10 +29,10 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+        <h4 class="modal-title">Veure imatge gran</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <img id="myImg" class="imgcent1" src="../images/pb_sf.jpg" alt="Placa base" style="width:100%;max-width:500px">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
