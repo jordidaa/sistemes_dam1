a5e9331f19cba758dcad3cdfa275878e10222ad5
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-10-09 17:31:13 +0200

    dd

--- components/tc_disc_dur.php
+++ components/tc_disc_dur.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Disc durs externs</title>
+        <title>Disc durs interns</title>
         <?php include ('../includes/meta_heder.php') ?>
     </head>    
     <body>
@@ -12,14 +12,13 @@
         <!--index-->
         <div class="center">
             <div class="pagination">
-            <a href="disc_intern.php" class="fletxas">&laquo; Disc + Connexions Integrades Internes de Disc Dur + Placa</a>
-                <a href="tc_disc_dur.php" class="fletxas">Disc + Targes de connexió de disc dur (connexió interna i/o externa) + 
-            Slots d’Expansió a la PB + Placa &raquo; </a>
+                <a href="disc_extern.php" class="fletxas">&laquo; Disc + Connexions Integrades Externes de Disc Dur + Placa</a>
+                <a href="#" class="fletxas"> Connexions de Video Integrades + Placa &raquo; </a>
             </div>
         </div>
-        <h1 class="titolcp">Disc + Connexions Integrades Externes de Disc Dur + Placa</h1>
+        <h1 class="titolcp">Disc + Connexions Integrades Internes de Disc Dur + Placa</h1>
         <div class="imgcent">
-            <img id="myImg" class="imgcent1" src="../images/pb_sf_usb.png" alt="Placa base" style="width:100%;max-width:400px">
+            <img id="myImg" class="imgcent1" src="../images/pb_sf.png" alt="Placa base" style="width:100%;max-width:400px">
         </div>
         <!-- Trigger the modal with a button -->
         <button type="button" class="btn btn-info btn-lg centbtn" data-toggle="modal" data-target="#myModal">Veure imatge gran</button>
@@ -35,7 +34,7 @@
                 <h4 class="modal-title">Placa Base</h4>
               </div>
               <div class="modal-body">
-                <img id="myImg" class="imgcentbtn" src="../images/pb_sf_usb.png" alt="Placa base" style="width:100%;max-width:600px">
+                <img id="myImg" class="imgcentbtn" src="../images/pb_sf.png" alt="Placa base" style="width:100%;max-width:600px">
               </div>
               <!--<div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -44,20 +43,15 @@
         
           </div>
         </div>
-        <h1 class="titolcp">USB</h1>
+        <h1 class="titolcp">M.2</h1>
         <div class="col-sm-5 col-md-6 cent md">
-          <img id="myImg" class="imgcent2" src="../images/usb.png" alt="Placa base" style="width:100%;max-width:250px">
+          <img id="myImg" class="imgcent2" src="../images/m_2.png" alt="Placa base" style="width:100%;max-width:250px">
         </div>
         <div class="col-sm-5 col-md-6 cent md">
-          <p>
-          Serveix per connectar perifèrics: teclat, ratoli, disc dur, usb ... <br>
-          Hi ha diferents tipus de usb que tenen diferents velocitats segons la versió:
-            <ul>
-              <li>El USB 2.0 (el de color negre) té una velocitat de 480 Mb/s.</li>
-              <li>El USB 3.2 gen 1 (el de color blau) té una velocitat de 4.8 Gb/s.</li>
-              <li>El USB 3.2 gen 2 (el de color vermell) té una velocitat de 10 Gb/s.</li>
-            </ul>
+          <p> 
+          El M.2 és la connexió de Disc dur amb placa base més ràpida que hi ha en el mercat, va a una velocitat de 6 Gbit/s.
           </p>
+
         <!-- Trigger the modal with a button -->
           <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal1">Veure imatge gran</button>
 
@@ -69,10 +63,10 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">USB</h4>
+                <h4 class="modal-title">M.2</h4>
               </div>
               <div class="modal-body">
-              <img id="myImg" class="imgcentbtn" src="../images/usb.png" alt="Placa base" style="width:100%;max-width:600px"> 
+              <img id="myImg" class="imgcentbtn" src="../images/m_2.png" alt="Placa base" style="width:100%;max-width:600px"> 
               </div>
               <!--<div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -82,10 +76,11 @@
           </div>
         </div>
         </div>
-        <h1 class="titolcp">Disc dur extern 3.1 gen 1</h1>
+        <h1 class="titolcp">SATA 3</h1>
         <div class="col-sm-5 col-md-6 cent md">
         <p>
-        Aquest disc dur 3.1 gen 1 va a una velocitat de 5000 Mbit/s.
+        El SATA 3 va a la mateixa velocitat que M.2 però perd velocitat perquè s'ha de connectar a partir d'un cable.
+        Va a una velocitat de 6 Gbit/s.
         </p>
         <!-- Trigger the modal with a button -->
           <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Veure imatge gran</button>
@@ -98,10 +93,10 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Disc dur extern 3.1 gen 1</h4>
+                <h4 class="modal-title">SATA 3</h4>
               </div>
               <div class="modal-body">
-              <img id="myImg" class="imgcentbtn" src="../images/dd_3_0.png" alt="Placa base" style="width:100%;max-width:600px"> 
+              <img id="myImg" class="imgcentbtn" src="../images/sata_3.png" alt="Placa base" style="width:100%;max-width:600px"> 
               </div>
               <!--<div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -112,18 +107,18 @@
         </div>
         </div>
         <div class="col-sm-5 col-md-6 cent md">
-          <img id="myImg" class="imgcent2" src="../images/dd_3_0.png" alt="Placa base" style="width:100%;max-width:250px">
+          <img id="myImg" class="imgcent2" src="../images/sata_3.png" alt="Placa base" style="width:100%;max-width:250px">
         </div>
-        <h1 class="titolcp">Disc dur extern 3.2</h1>
+        <h1 class="titolcp">Disc dur M.2</h1>
         <div class="col-sm-5 col-md-6 cent md">
-          <img id="myImg" class="imgcent2" src="../images/dd_3_2.png" alt="Placa base" style="width:100%;max-width:250px">
+          <img id="myImg" class="imgcent2" src="../images/m_2_dd.png" alt="Placa base" style="width:100%;max-width:250px">
         </div>
         <div class="col-sm-5 col-md-6 cent md">
           <p> 
-          Aquest disc dur extern 3.2 té diferents velocitats d'escriptura i lectura:
+          Aquest disc dur M.2 té diferents velocitats d'escriptura i lectura:
             <ul>
-              <li>Té una velocitat d'escriptura de 1000 MB/s.</li>
-              <li>Té una velocitat de lectura de 1050 MB/s.</li>
+              <li>Té una velocitat d'escriptura de 530 MB/s.</li>
+              <li>Té una velocitat de lectura de 560 MB/s.</li>
             </ul>
           </p>
 
@@ -138,10 +133,10 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Disc dur extern 3.2</h4>
+                <h4 class="modal-title">Disc dur M.2</h4>
               </div>
               <div class="modal-body">
-              <img id="myImg" class="imgcentbtn" src="../images/dd_3_2.png" alt="Placa base" style="width:100%;max-width:600px"> 
+              <img id="myImg" class="imgcentbtn" src="../images/m_2_dd.png" alt="Placa base" style="width:100%;max-width:600px"> 
               </div>
               <!--<div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -151,7 +146,7 @@
           </div>
         </div>
         </div>
-        <!--<h1 class="titolcp">Disc dur SATA 3</h1>
+        <h1 class="titolcp">Disc dur SATA 3</h1>
         <div class="col-sm-5 col-md-6 cent md">
         <p>
         Aquest disc dur SATA 3 té diferents velocitats de lectura, escriptura i a les revolucions que van:
@@ -159,34 +154,34 @@
               <li>Té una velocitat d'escriptura i de lectura de 150 MB/s.</li>
               <li>El disc dur va a 7200 revolucions per minut.</li>
             </ul>
-        </p>-->
+        </p>
         <!-- Trigger the modal with a button -->
-          <!--<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4">Veure imatge gran</button>-->
+          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4">Veure imatge gran</button>
 
         <!-- Modal -->
-        <!--<div class="modal fade" id="myModal4" role="dialog">
-          <div class="modal-dialog">-->
+        <div class="modal fade" id="myModal4" role="dialog">
+          <div class="modal-dialog">
 
             <!-- Modal content-->
-            <!--<div class="modal-content">
+            <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Disc dur SATA 3</h4>
               </div>
               <div class="modal-body">
               <img id="myImg" class="imgcentbtn" src="../images/sata_3_dd.png" alt="Placa base" style="width:100%;max-width:600px"> 
-              </div>-->
+              </div>
               <!--<div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>-->
-            <!--</div>
+            </div>
 
           </div>
         </div>
         </div>
         <div class="col-sm-5 col-md-6 cent md">
           <img id="myImg" class="imgcent2" src="../images/sata_3_dd.png" alt="Placa base" style="width:100%;max-width:250px">
-        </div>-->
+        </div>
     </body>
     <?php include ('../includes/meta_footer.php') ?>
 </html>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <p class="index"><a href="components/slots_expancio.php">Slots d’Expansió a la PB + Placa</a></p>
         <p class="index"><a href="components/disc_intern.php">Disc + Connexions Integrades Internes de Disc Dur + Placa</a></p>
         <p class="index"><a href="components/disc_extern.php">Disc + Connexions Integrades Externes de Disc Dur + Placa</a></p>
-        <p class="index"><a href="">Disc + Targes de connexió de disc dur (connexió interna i/o externa) + 
+        <p class="index"><a href="components/tc_disc_dur.php">Disc + Targes de connexió de disc dur (connexió interna i/o externa) + 
             Slots d’Expansió a la PB + Placa</a></p>
         <p class="index"><a href="">Connexions de Video Integrades + Placa</a></p>
         <p class="index"><a href="">Targes de Video + Slots d’Expansió a la PB + Placa</a></p>
