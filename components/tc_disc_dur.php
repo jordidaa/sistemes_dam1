bfe41712903cfdb686877f839cadc24a928101f9
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-10-09 17:53:29 +0200

    Update tc_disc_dur.php

--- components/tc_disc_dur.php
+++ components/tc_disc_dur.php
@@ -147,7 +147,7 @@
           </div>
         </div>
         </div>
-        <h1 class="titolcp">Disc dur SATA 3</h1>
+        <!--<h1 class="titolcp">Disc dur SATA 3</h1>
         <div class="col-sm-5 col-md-6 cent md">
         <p>
         Aquest disc dur SATA 3 té diferents velocitats de lectura, escriptura i a les revolucions que van:
@@ -156,15 +156,15 @@
               <li>El disc dur va a 7200 revolucions per minut.</li>
             </ul>
         </p>
-        <!-- Trigger the modal with a button -->
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4">Veure imatge gran</button>
+         Trigger the modal with a button -->
+        <!--  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4">Veure imatge gran</button>
 
-        <!-- Modal -->
-        <div class="modal fade" id="myModal4" role="dialog">
+         Modal -->
+        <!--<div class="modal fade" id="myModal4" role="dialog">
           <div class="modal-dialog">
 
-            <!-- Modal content-->
-            <div class="modal-content">
+             Modal content-->
+            <!--<div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Disc dur SATA 3</h4>
@@ -172,9 +172,9 @@
               <div class="modal-body">
               <img id="myImg" class="imgcentbtn" src="../images/sata_3_dd.png" alt="Placa base" style="width:100%;max-width:600px"> 
               </div>
-              <!--<div class="modal-footer">
+              <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              </div>-->
+              </div>
             </div>
 
           </div>
@@ -182,7 +182,7 @@
         </div>
         <div class="col-sm-5 col-md-6 cent md">
           <img id="myImg" class="imgcent2" src="../images/sata_3_dd.png" alt="Placa base" style="width:100%;max-width:250px">
-        </div>
+        </div>-->
     </body>
     <?php include ('../includes/meta_footer.php') ?>
 </html>
