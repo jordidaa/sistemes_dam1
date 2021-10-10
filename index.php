b3d52c0ef2f4e73a413a4aa5b50d664b50bacbdc
jordidaa <72455782+jordidaa@users.noreply.github.com>
2021-10-10 07:46:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
         <p class="index"><a href="components/tc_disc_dur.php">Disc + Targes de connexió de disc dur (connexió interna i/o externa) + 
             Slots d’Expansió a la PB + Placa</a></p>
         <p class="index"><a href="components/video_int.php">Connexions de Video Integrades + Placa</a></p>
-        <p class="index"><a href="tg_video.php">Targes de Video + Slots d’Expansió a la PB + Placa</a></p>
-        <p class="index"><a href="audio.php">Connexions d’Audio Integrades (Tipus, color) + Placa </a></p>
-        <p class="index"><a href="tg_comunicacio.php">Targes de Comunicació + Slots d’Expansió a la PB + Placa</a></p>
+        <p class="index"><a href="components/tg_video.php">Targes de Video + Slots d’Expansió a la PB + Placa</a></p>
+        <p class="index"><a href="components/audio.php">Connexions d’Audio Integrades (Tipus, color) + Placa </a></p>
+        <p class="index"><a href="components/tg_comunicacio.php">Targes de Comunicació + Slots d’Expansió a la PB + Placa</a></p>
         <p class="index"><a href="">Connexions Comunicació Integrades a la placa + placa</a></p>
         <p class="index"><a href="">Adaptadors de Comunicació a partir de USB</a></p>
         <p class="index"><a href="components/disc_extern.php">Tipus de USB (Velocitat, Color) + Placa (Estan explicats a Disc + Connexions Integrades Externes de Disc Dur + Placa)</a></p>
